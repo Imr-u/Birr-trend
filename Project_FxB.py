3c141dbe90bc6823c13c99fb534641507d389ac2
Emran <imruabubeker@gmail.com>
2025-09-07 16:45:02 +0300

    local change

--- Project_FxB.py
+++ Project_FxB.py
@@ -24,50 +24,8 @@ page = requests.get(url)
 
 data = page.json()
 
-usd_entry = next(item for item in data["data"] if item["currency"]["code"] == "EUR")
 
-eur_buying = float(usd_entry["buying"])
-eur_selling = float(usd_entry["selling"])
-eur_weighted = float(usd_entry["weighted_average"])
-scrape_time = datetime.date.today()
-
-print( eur_buying)
-print( eur_selling)
-print( eur_weighted)
-print(scrape_time)
-
-rates= [[eur_buying, eur_selling, eur_weighted, scrape_time]]
-columns = ["buying", "selling", "avg", "scrape_time"]
-
-
-# In[5]:
-
-
-df= pd.DataFrame(rates,columns= columns)
-print(df)
-
-df_new = pd.DataFrame(rates, columns = columns)
-
-
-# In[8]:
-
-
-if os.path.exists("EURBIRR.csv"):
-    df_old = pd.read_csv("EURBIRR.csv")
-else:
-    df_old= pd.DataFrame(columns=df_new.columns)
-
-df_combined = pd.concat([df_old, df_new], ignore_index=True)
-df_clean = df_combined.drop_duplicates(subset=["scrape_time"])
-
-
-df_clean.to_csv("EURBIRR.csv", index=False) 
-
-
-# In[17]:
-
-
-usd_entry = next(item for item in data["data"] if item["currency"]["code"] == "AED")
+aed_entry = next(item for item in data["data"] if item["currency"]["code"] == "AED")
 
 aed_buying = float(usd_entry["buying"])
 aed_selling = float(usd_entry["selling"])
